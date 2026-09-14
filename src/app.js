742595f6f093cfa9a6289dd6a1d89c8a5be692cd
pabloUribe2511 <pablo.uribe2511@gmail.com>
2026-09-14 13:37:42 -0500

    Configuracion variables de entorno en el proyecto

--- src/app.js
+++ src/app.js
@@ -11,3 +11,15 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
+
+const { port } = require('./config/env'); // Import the port from the env file
+
+// Inicializacion del servidor y primera ruta
+app.get("/", (req, res) => {
+  res.send("Hola mi server en Express");
+});
+
+// Inicio del servidor
+app.listen(port, () => {
+  console.log(`Servidor corriendo en http://localhost:${port}`);
+});
